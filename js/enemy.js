73062a24f871cfebd29d2f5eb51c361e27a7d7f5
Mace <e.schlinkmeier@web.de>
2021-10-02 20:43:07 +0200

    Added Enemy and Player. Changed to Function.

--- js/enemy.js
+++ js/enemy.js
@@ -1,1 +1,7 @@
-export const enemy = {}
+export const enemy = ({ player, enemy }) => ({
+
+
+
+
+
+})
--- js/player.js
+++ js/player.js
@@ -1,1 +1,86 @@
-export const player = {}
+export const player = ({ player, enemy }) => ({
+
+  attackSword:{
+      text: 'The player whirls his sword through the air and strikes.',
+      resutl: {
+          miss: ['dodge right', 'dodge left', 'duck'],
+          critical: 'big'],
+          hit: 'big']
+      },
+      damageFrom: 20,
+      damageTo: 50
+  },
+  attackBareHands:{
+        text: 'red',
+        resutl: {
+            miss: 'big',
+            critical: true,
+            hit: 5
+        },
+        damageFrom: 20,
+        damageTo: 50
+  },
+  attackFoot:{
+        text: 'red',
+        resutl: {
+            miss: 'big',
+            critical: true,
+            hit: 5
+        },
+        damageFrom: 20,
+        damageTo: 50
+  },
+  attackGun:{
+        text: 'red',
+        resutl: {
+            miss: 'big',
+            critical: true,
+            hit: 5
+        },
+        damageFrom: 20,
+        damageTo: 50
+  },
+  attackJumpKick:{
+        text: 'red',
+        resutl: {
+            miss: 'big',
+            critical: true,
+            hit: 5
+        },
+        damageFrom: 20,
+        damageTo: 50
+  },
+  attackSmashBareHands:{
+        text: 'red',
+        resutl: {
+            miss: 'big',
+            critical: true,
+            hit: 5
+        },
+        damageFrom: 20,
+        damageTo: 50
+  },
+  attackStompEarthQuacke:{
+        text: 'red',
+        resutl: {
+            miss: 'big',
+            critical: true,
+            hit: 5
+        },
+        damageFrom: 20,
+        damageTo: 50
+  },
+  attackScratch:{
+        text: 'The ${state.pName}',
+        resutl: {
+            miss: 'big',
+            critical: true,
+            hit: 5
+        },
+        damageFrom: 20,
+        damageTo: 50
+  },
+
+
+
+  })
